4df59fe8dc6aa50eb2dce0c8e3ba6ef8ff76fa47
ardayigit <ardayig725@gmail.com>
2023-12-23 20:04:49 +0300

    Initial Commit

---
+++ Kısa_Sınav_2_Arda_Yigit_1230505022/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Kısa_Sınav_2_Arda_Yigit_1230505022
+//
+//  Created by Arda Yiğit on 23.12.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
